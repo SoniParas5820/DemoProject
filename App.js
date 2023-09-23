043da0b52b3c372af5aaff2b4fcf9e2b26243b9c
SoniParas5820 <soniparas97145@gmail.com>
2023-09-23 16:59:56 +0530

    CodeStructure

---
+++ App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import type {PropsWithChildren} from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+const App = () => {
+  return (
+    
+  );
+}
+
+export default App;
